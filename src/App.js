93426d9839a817dfe649af8d178015bbe43f55a2
unknown <hbpsnape@yahoo.com>
2021-06-16 11:41:48 +0600

    onButtonSubmit converted to arrow fn

--- src/App.js
+++ src/App.js
@@ -48,16 +48,11 @@ class App extends Component {
       app.models
       .predict(
         Clarifai.FACE_DETECT_MODEL, 
-        this.state.input)
-      .then(
-      function(response) {
-        // console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-        this.calculateFaceLocation(response);
-      },
-      function(err) {
-        // body...
-      }
-    );
+        this.state.input
+      )
+      .then(response => this.calculateFaceLocation(response))
+      .catch(err => console.log(err));
+      
   }
 
  
